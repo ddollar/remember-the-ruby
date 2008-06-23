9c6001a79bf92ad1575e54fcccf80b6ac2a8e81a
David Dollar <ddollar@gmail.com>
2008-06-23 15:57:05 -0400

    bumped to 0.2.1 to try to get github to build the gem

--- lib/remember-the-ruby.rb
+++ lib/remember-the-ruby.rb
@@ -5,7 +5,7 @@ unless defined? RememberTheRuby
 module RememberTheRuby
 
   # :stopdoc:
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
   LIBPATH = File.expand_path(File.dirname(__FILE__)) + File::SEPARATOR
   PATH = File.dirname(LIBPATH) + File::SEPARATOR
   # :startdoc:
